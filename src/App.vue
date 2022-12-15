7ab70911c5f1bf4326a44af18c4589dff9f8703f
Anthony Quisenberry <anthony.quisenberry@L121-202105-01.local>
2022-12-15 15:30:43 -0500

    complete navigation

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <DogWorldNavigation />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import DogWorldNavigation from "./components/DogWorldNavigation.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    DogWorldNavigation,
   },
 };
 </script>
 
 <style lang="scss">
+$font: "Rubik";
+@font-face {
+  font-family: $font;
+  font-style: normal;
+  font-weight: 300;
+  src: url("./assets/fonts/Rubik-Light.ttf") format("ttf");
+}
+@font-face {
+  font-family: $font;
+  font-style: normal;
+  font-weight: 400;
+  src: url("./assets/fonts/Rubik-Medium.ttf") format("ttf");
+}
+@font-face {
+  font-family: $font;
+  font-style: normal;
+  font-weight: 500;
+  src: url("./assets/fonts/Rubik-Regular.ttf") format("ttf");
+}
+@font-face {
+  font-family: $font;
+  font-style: normal;
+  font-weight: 700;
+  src: url("./assets/fonts/Rubik-Bold.ttf") format("ttf");
+}
+* {
+  font: $font;
+  box-sizing: border-box;
+}
+a {
+  text-decoration: none;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/DogWorldNavigation.vue
@@ -0,0 +1,120 @@
+<template>
+  <ul class="navigation">
+    <li class="navigation__logo">
+      <a href="#home" aria-label="go to dog world home">
+        <img
+          :src="require('../assets/img/dog-world-logo.svg')"
+          alt="Dog Word Logo"
+        />
+      </a>
+    </li>
+    <li class="navigation__links-container">
+      <div class="navigation__hamburger" @click="toggleMenu">
+        <img :src="require('../assets/img/icon-hamburger.svg')" alt="" />
+      </div>
+      <ul class="navigation__links" :class="hideMenu ? 'mobile-hide' : ''">
+        <li class="navigation__link">
+          <a href="#" aria-label="home">Home</a>
+        </li>
+        <li class="navigation__link">
+          <a href="#about-us" aria-label="about us">About us</a>
+        </li>
+        <li class="navigation__link">
+          <a href="#pricing" aria-label="pricing">Pricing</a>
+        </li>
+      </ul>
+    </li>
+    <li class="navigation__button">
+      <a href="#lets-talk" aria-label="let's talk">Let's Talk </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const hideMenu = ref(true);
+
+    const toggleMenu = () => {
+      hideMenu.value = !hideMenu.value;
+    };
+    return {
+      toggleMenu,
+      hideMenu,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$copper: #9a6d4a;
+$white: #ffffff;
+$dark-gray: #333333;
+.navigation {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: center;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  background-color: $white;
+  &__logo {
+    flex: 1;
+  }
+  &__links-container {
+    flex: 4;
+  }
+  &__links {
+    list-style: none;
+    padding: 0;
+    a {
+      color: $dark-gray;
+    }
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+  }
+  &__link {
+    padding: 16px;
+    font-weight: 500;
+    font-size: 16px;
+  }
+  &__button {
+    display: inline-block;
+    padding: 16px 28px;
+    background-color: $copper;
+    border-radius: 30px;
+    font-weight: 500;
+    a {
+      color: $white;
+    }
+  }
+}
+.mobile-hide {
+  display: none;
+}
+@media screen and (min-width: 768px) {
+  .navigation {
+    max-width: 1010px;
+    margin: auto;
+    &__hamburger {
+      display: none;
+    }
+    &__links {
+      display: inline-block;
+      flex-direction: row;
+      justify-content: flex-end;
+      margin-right: 50px;
+    }
+    &__link {
+      padding: 10px;
+    }
+  }
+  .mobile-hide {
+    display: flex;
+  }
+}
+</style>
